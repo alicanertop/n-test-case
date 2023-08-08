4284f198e41438805e9c881ca184cc693749c50d
Alican ERTOP <alican.ertop@nesine.com>
2023-08-08 10:12:39 +0300

    useCtxState updated
    selector support added
    only key support will add later again

--- src/services/createContext/createContext.tsx
+++ src/services/createContext/createContext.tsx
@@ -5,29 +5,36 @@ import {
   Reducer,
   useContext,
   useReducer
-} from 'react';
+} from 'react'
 
 export function createContext<StateType, ActionType>(
   reducer: Reducer<StateType, ActionType>,
   initialState: StateType
 ) {
-  const defaultDispatch: Dispatch<ActionType> = () => initialState;
-  const stateCtx = createCtx(initialState);
-  const dispatchCtx = createCtx(defaultDispatch);
+  const defaultDispatch: Dispatch<ActionType> = () => initialState
+  const stateCtx = createCtx(initialState)
+  const dispatchCtx = createCtx(defaultDispatch)
 
-  const useCtxAllState = () => useContext(stateCtx);
-  const useCtxDispatch = () => useContext(dispatchCtx);
+  const useCtxAllState = () => useContext(stateCtx)
+  const useCtxDispatch = () => useContext(dispatchCtx)
 
-  /** Only one depth selector for comparison */
-  const useCtxState = <K extends keyof StateType>(property: K) => useContext(stateCtx)[property];
+  // /** Only one depth selector for comparison */
+  // function useCtxState<K extends keyof StateType>(property: K) {
+  //   return useContext(stateCtx)[property]
+  // }
+
+  /** Using selector to index data */
+  function useCtxState<SO>(selector: (state: StateType) => SO) {
+    return selector(useContext(stateCtx))
+  }
 
   function CtxProvider({ children }: PropsWithChildren<{}>) {
-    const [state, dispatch] = useReducer<Reducer<StateType, ActionType>>(reducer, initialState);
+    const [state, dispatch] = useReducer<Reducer<StateType, ActionType>>(reducer, initialState)
     return (
       <dispatchCtx.Provider value={dispatch}>
         <stateCtx.Provider value={state}>{children}</stateCtx.Provider>
       </dispatchCtx.Provider>
-    );
+    )
   }
-  return { useCtxState, useCtxAllState, useCtxDispatch, CtxProvider } as const;
+  return { useCtxState, useCtxAllState, useCtxDispatch, CtxProvider } as const
 }
