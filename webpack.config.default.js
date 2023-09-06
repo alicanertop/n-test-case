165a6509d6dbe858732d21c7dc8d16532a33f12f
Alican ERTOP <alican.ertop@nesine.com>
2023-09-06 11:25:51 +0300

    .js .jsx support & husky precommit add

--- webpack.config.default.js
+++ webpack.config.default.js
@@ -46,6 +46,13 @@ const defaultConfigs = {
   module: {
     rules: [
       {
+        test: /\.(js|jsx)$/,
+        exclude: [/node_modules/],
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
         // Typescript loader
         test: /\.tsx?$/,
         exclude: /(node_modules|\.webpack)/,
